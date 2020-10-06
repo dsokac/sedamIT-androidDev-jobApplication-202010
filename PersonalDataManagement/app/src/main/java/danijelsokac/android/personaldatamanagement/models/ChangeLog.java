f3e42cce9cf8a8dbdb035503fc675ef391efb9b9
danijelsokac92 <danijel.sokac92@gmail.com>
2020-10-06 15:52:47 +0200

    add entities

--- PersonalDataManagement/app/src/main/java/danijelsokac/android/personaldatamanagement/models/ChangeLog.java
+++ PersonalDataManagement/app/src/main/java/danijelsokac/android/personaldatamanagement/models/ChangeLog.java
@@ -1,4 +1,23 @@
 package danijelsokac.android.personaldatamanagement.models;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity(tableName = "change_logs")
+@Getter @Setter
 public class ChangeLog {
+
+    static final String DELETE = "DELETE";
+    static final String UPDATE = "UPDATE";
+    static final String CREATE = "CREATE";
+
+    @PrimaryKey(autoGenerate = true) private int id;
+    @ColumnInfo(name = "local_id")  private int localId;
+    @ColumnInfo(name = "remote_id")  private int remoteId;
+    private String action; //DELETE, UPDATE, CREATE;
+
 }
--- PersonalDataManagement/app/src/main/java/danijelsokac/android/personaldatamanagement/models/UserModel.java
+++ PersonalDataManagement/app/src/main/java/danijelsokac/android/personaldatamanagement/models/UserModel.java
@@ -1,5 +1,9 @@
 package danijelsokac.android.personaldatamanagement.models;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
@@ -8,13 +12,15 @@ import java.util.Objects;
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity(tableName = "app_users")
 @Getter @Setter
 public class UserModel {
+    @PrimaryKey(autoGenerate = true)  @ColumnInfo(name = "local_id")  private int localId;
     @Expose                            private int id;
     @Expose @SerializedName("ime")     private String name;
     @Expose @SerializedName("prezime") private String surname;
-    @Expose @SerializedName("telefon") private String phoneNumber;
-    @Expose @SerializedName("mobitel") private String mobilePhoneNumber;
+    @ColumnInfo(name = "phone_number") @Expose @SerializedName("telefon") private String phoneNumber;
+    @ColumnInfo(name = "mobile_number") @Expose @SerializedName("mobitel") private String mobilePhoneNumber;
     @Expose @SerializedName("mail")    private String email;
     @Expose @SerializedName("adresa")  private String address;
 
